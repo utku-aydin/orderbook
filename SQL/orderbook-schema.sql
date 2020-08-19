fcfc4c16d6e43b70e33d55b6e36c90aa6533edef
Utku Aydin <30289497+Brodican@users.noreply.github.com>
2020-08-19 17:30:19 +0300

    Update orderbook-schema.sql

--- SQL/orderbook-schema.sql
+++ SQL/orderbook-schema.sql
@@ -5,15 +5,18 @@ CREATE DATABASE orderbookdb;
 USE orderbookdb;
 
 CREATE TABLE ob_order (
-	id INT primary key AUTO_INCREMENT,
-    symbol VARCHAR(8) NOT NULL,
+	id 				INT 		AUTO_INCREMENT,
+	version_id 		INT 		DEFAULT 0,
+    symbol 			VARCHAR(8) 	NOT NULL,
 	price DECIMAL(10,2) NOT NULL,
 	orderSize INT NOT NULL,
 	side ENUM('BUY', 'SELL') NOT NULL,
     numberMatched INT NOT NULL,
     placedAt DATETIME NOT NULL,
 	userSymbol VARCHAR(8) NOT NULL,
-	status ENUM('ACTIVE', 'CANCELLED', 'FULFILLED') NOT NULL
+	status ENUM('ACTIVE', 'CANCELLED', 'FULFILLED') NOT NULL,
+    CONSTRAINT PK_OB_ORDER 
+    	PRIMARY KEY (id, version_id)
 );
 
 CREATE TABLE trade (
@@ -27,7 +30,7 @@ CREATE TABLE trade (
     	FOREIGN KEY (buyId)
     	REFERENCES ob_order(id),
     CONSTRAINT FK_trade_buyId
-    	FOREIGN KEY (FormatID)
+    	FOREIGN KEY (sellId)
     	REFERENCES ob_order(id)
 );
 

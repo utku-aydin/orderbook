a8a121d8b8bde74bac725102d856def09534ebe5
Utku Aydin <30289497+Brodican@users.noreply.github.com>
2020-08-21 17:45:12 +0300

    Update orderbook-schema.sql

--- SQL/orderbook-schema.sql
+++ SQL/orderbook-schema.sql
@@ -1,28 +1,33 @@
 DROP DATABASE IF EXISTS orderbookdb;
-
 CREATE DATABASE orderbookdb;
-
 USE orderbookdb;
-
+CREATE TABLE ob_stock (
+	id 				INT 			primary key 	AUTO_INCREMENT,
+	tick_size		DECIMAL(2,2)	NOT NULL,
+	stock_symbol	VARCHAR(8)		NOT NULL
+);
+CREATE TABLE ob_company (
+	id 				INT 			primary key 	AUTO_INCREMENT,
+	company_symbol	VARCHAR(8)		NOT NULL
+);
 CREATE TABLE ob_order (
 	id 				INT 													AUTO_INCREMENT,
 	version 		INT 													DEFAULT 0,
-    stock_id		INT		 												NOT NULL,
+    stock_id		INT 												NOT NULL,
 	price 			DECIMAL(10,2) 											NOT NULL,
 	order_size 		INT 													NOT NULL,
-	side 			ENUM('BUY', 'SELL') 									NOT NULL,
+	side 			ENUM(‘BUY’, ‘SELL’) 									NOT NULL,
     number_matched 	INT 													NOT NULL,
     placed_at 		DATETIME 												NOT NULL,
-	user_symbol 	VARCHAR(8) 												NOT NULL,
-	status 			ENUM('PENDING', 'ACTIVE', 'CANCELLED', 'FULFILLED') 	NOT NULL,
+	user_symbol 		VARCHAR(8) 												NOT NULL,
+	status 			ENUM(‘PENDING’, ‘ACTIVE’, ‘CANCELLED’, ‘FULFILLED’) 	NOT NULL,
 	
-    CONSTRAINT PK_ob_order 
+    CONSTRAINT PK_ob_order
     	PRIMARY KEY (id, version),
 	CONSTRAINT FK_ob_order_stock
 		FOREIGN KEY (stock_id)
 		REFERENCES ob_stock(id)
 );
-
 CREATE TABLE ob_trade (
 	id 				INT 		primary key 	AUTO_INCREMENT,
     buy_id 			INT 		NOT NULL,
@@ -40,13 +45,6 @@ CREATE TABLE ob_trade (
     	FOREIGN KEY (sell_id, sell_version)
     	REFERENCES ob_order(id, version)
 );
-
-CREATE TABLE ob_stock (
-	id 				INT 			primary key 	AUTO_INCREMENT,
-	tick_size		DECIMAL(2,2)	NOT NULL,
-	stock_symbol	VARCHAR(8)		NOT NULL
-);
-
 CREATE TABLE ob_user (
 	id 				INT 			primary key 	AUTO_INCREMENT,
 	company_id		INT				NOT NULL,
@@ -56,24 +54,14 @@ CREATE TABLE ob_user (
 		FOREIGN KEY (company_id)
 		REFERENCES ob_company(id)
 );
-
-CREATE TABLE ob_company (
-	id 				INT 			primary key 	AUTO_INCREMENT,
-	company_symbol	VARCHAR(8)		NOT NULL
-);
-
 CREATE TABLE ob_trade_audit (
 	id 				INT 			primary key 	AUTO_INCREMENT,
 	trade_log		TEXT			NOT NULL
 );
-
 CREATE TABLE ob_order_audit (
 	id 				INT 			primary key 	AUTO_INCREMENT,
 	order_log		TEXT			NOT NULL
 );
-
 DROP DATABASE IF EXISTS orderbookdbtest;
-
 CREATE DATABASE orderbookdbtest;
-
 USE orderbookdbtest;

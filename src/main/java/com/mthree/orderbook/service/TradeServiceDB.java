287583e2a39138998dcab641ea368de0df63e502
Utku Aydin <30289497+Brodican@users.noreply.github.com>
2020-08-24 17:58:29 +0300

    Price list working somewhat

--- src/main/java/com/mthree/orderbook/service/TradeServiceDB.java
+++ src/main/java/com/mthree/orderbook/service/TradeServiceDB.java
@@ -29,6 +29,16 @@ public class TradeServiceDB implements TradeService {
         if(trades.isEmpty()) {
             return prices;
         }
+        
+        ChronoUnit comparison = ChronoUnit.SECONDS;
+        
+        if (interval > 60) {
+            comparison = ChronoUnit.MINUTES;
+        } else if (interval > 3600) {
+            comparison = ChronoUnit.HOURS;
+        } else if (interval > 86400) {
+            comparison = ChronoUnit.DAYS;
+        }
 
         prices.add(trades.get(0).getTrade_price());
         int checked = 1;
@@ -39,8 +49,8 @@ public class TradeServiceDB implements TradeService {
             }
             Trade t = trades.get(checked++);
             //if there's a trade at that time, add its price
-            if(LocalDateTime.now().minusSeconds(interval*checked).truncatedTo(ChronoUnit.SECONDS)
-                    .isEqual(t.getTrade_time().truncatedTo(ChronoUnit.SECONDS))) {
+            if(LocalDateTime.now().minusSeconds(interval*checked).truncatedTo(comparison)
+                    .isEqual(t.getTrade_time().truncatedTo(comparison))) {
                 prices.add(t.getTrade_price());
             }
             //otherwise, add the price of the previous trade
@@ -52,8 +62,11 @@ public class TradeServiceDB implements TradeService {
         //if there was no data available at a certain time, set the amount to the previously occurred trade
         while(prices.contains(new BigDecimal("-1"))) {
             int index = prices.indexOf(new BigDecimal("-1"));
-            
-            prices.set(index, prices.get(index+1));
+            if (index < prices.size() - 1) {
+                prices.set(index, prices.get(index+1));
+            } else {
+                break;
+            }
         }
 
         //if there is a need, add zeroes to match the needed size

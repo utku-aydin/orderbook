44bcb265b5e40fb374e9ca1cf2129adb18350a4c
Utku Aydin <30289497+Brodican@users.noreply.github.com>
2020-08-24 17:00:29 +0300

    Trade

--- src/main/java/com/mthree/orderbook/service/TradeServiceDB.java
+++ src/main/java/com/mthree/orderbook/service/TradeServiceDB.java
@@ -6,8 +6,10 @@ import com.mthree.orderbook.repository.TradeRepository;
 import java.math.BigDecimal;
 import java.time.DayOfWeek;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.data.domain.Sort;
 
 public class TradeServiceDB implements TradeService {
 
@@ -18,50 +20,32 @@ public class TradeServiceDB implements TradeService {
     }
 
     @Override
-    public BigDecimal[] getPricesWithStep(int days) {
-        LocalDateTime neededDate = LocalDateTime.now().withHour(0).withMinute(0).withSecond(0).withNano(0).minusDays(days);
-
-        switch(days) {
-            //return one price per hour
-            case 1:
-                BigDecimal[] prices = new BigDecimal[8];
-                int neededHour = 9;
-                while(neededHour < 17) {
-                    for (Trade t : tradeRepository.findTradesFromDate(neededDate)) {
-                        if (neededHour > 17) {
-                            break;
-                        }
-                        if (t.getTrade_time().getHour() == neededHour) {
-                            prices[neededHour - 9] = t.getTrade_price();
-                            neededHour++;
-                        }
-                    }
-                    neededHour++;
-                }
-            //return one price at around 9 and one around 5 for each day
-            //excluding weekend
-            case 7:
-               prices = new BigDecimal[10];
-               int totalFound = 0;
-
-               while(totalFound < 10) {
-                   for(Trade t : tradeRepository.findTradesFromDate(neededDate)) {
-                       if(t.getTrade_time().getDayOfWeek().equals(DayOfWeek.SATURDAY) || t.getTrade_time().getDayOfWeek().equals(DayOfWeek.SUNDAY)) {
-                           continue;
-                       }
-                       if(totalFound == 10) {
-                           break;
-                       }
-                       
-                   }
-               }
-            //return one price per day
-            //excluding weekends
-            case 30:
-
+    public List<BigDecimal> getPricesWithStep(int interval, int count) {
+        List<Trade> trades = tradeRepository.findAll(Sort.by(Sort.Direction.DESC, "trade_time"));
+        List<BigDecimal> prices = new ArrayList<>();
+        prices.add(trades.get(0).getTrade_price());
+        int checked = 1;
+        while(checked < count) {
+            //if all trades have been checked, stop and return
+            if(checked >= trades.size()) {
+                break;
+            }
+            Trade t = trades.get(checked++);
+            //if there's a trade at that time, add its price
+            if(LocalDateTime.now().minusSeconds(interval*checked).truncatedTo(ChronoUnit.SECONDS)
+                    .isEqual(t.getTrade_time().truncatedTo(ChronoUnit.SECONDS))) {
+                prices.add(t.getTrade_price());
+            }
+            //otherwise, add the price of the previous trade
+            else {
+                prices.add(prices.get(prices.size()-1));
+            }
         }
-
-        return null;
+        //if there is a need, add zeroes to match the needed size
+        while(prices.size() < count) {
+            prices.add(new BigDecimal("0"));
+        }
+        return prices;
     }
     
 }

95c26fcf4046ba3bbbfe62454bf9b7b8857ed52e
Alex Morrison <alex.ranald.morrison@gmail.com>
2020-08-25 16:07:15 +0100

    tr

--- Front_End/order-book/src/Components/TickerFeed.js
+++ Front_End/order-book/src/Components/TickerFeed.js
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Ticker from 'react-ticker'
 
-function TickerFeed (){
-    return <p>Ticker Feed</p>
-}
+const GetTradesFromAPI = () =>{
+    const [trades,setTrades ] = useState("");
+    useEffect(() => {
+        async function fetchData(){
+            const tradesFromAPI = await fetch("http://localhost:8080/api/buyOrders")
+            console.log("Trades from API are " + tradesFromAPI)
+            setTrades(tradesFromAPI);
+        }
+        fetchData();
+    }, []);
+    console.log("trades are:" + trades);
+    return trades ? (
+        <p style={{whiteSpace: "nowrap"}}>{trades} +++</p>
+    ) : (<p style={{ visibility: "hidden"}}>Placeholder</p>
+    );
+     
+};
+
+
+const TickerFeed = () => (
+    <Ticker>
+        {({ index }) => (
+            <>
+                <h1>This is the Headline of element #{index}!</h1>
+                <img src="www.my-image-source.com/" alt=""/>
+            </>
+        )}
+    </Ticker>
+)
 
 export default TickerFeed

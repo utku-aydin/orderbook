75745b4f8a3312aa8331b46de16c9fe53a911279
Stanislav Minchev <siskoooo75@gmail.com>
2020-08-26 16:57:10 +0300

    testing/cleaning done

--- src/main/java/com/mthree/orderbook/service/UserServiceDB.java
+++ src/main/java/com/mthree/orderbook/service/UserServiceDB.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mthree.orderbook.service;
 
 import com.mthree.orderbook.entity.User;
@@ -10,10 +5,6 @@ import com.mthree.orderbook.repository.UserRepository;
 import java.util.List;
 import org.springframework.stereotype.Repository;
 
-/**
- *
- * @author utkua
- */
 @Repository
 public class UserServiceDB implements UserService {
     

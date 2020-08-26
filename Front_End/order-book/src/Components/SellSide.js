a9af6da212612e6667828c848e22d69854a635a3
Alex Morrison <alex.ranald.morrison@gmail.com>
2020-08-26 16:53:30 +0100

    updated sellside

--- Front_End/order-book/src/Components/SellSide.js
+++ Front_End/order-book/src/Components/SellSide.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Table, Button } from "react-bootstrap";
+import SellSideRow from "./SellSideRow"
 
 
 const SellSideHeader = () => {
@@ -9,26 +10,59 @@ const SellSideHeader = () => {
             <th>quantity</th>
             <th>total</th>
             <th>Symbol</th>
+            <th>id</th>
             <th>Cancel</th>
         </tr>
     );
 }
 
-const SellSideRow = ({ order }) => {
-    let {price,order_size,number_matched,side,stock} = order;
-    let quantity = order_size - number_matched;
+// const SellSideRow = ({ order }) => {
+//     let {price,order_size,number_matched,side,stock} = order;
+//     let quantity = order_size - number_matched;
 
-    return (<tr>
-        <td>{price}</td>
-        <td>{quantity}</td>
-        <td>{price * quantity}</td>
-        <td>{stock.stock_symbol}</td>
-        <td><Button>Cancel</Button></td>
-    </tr>
-    );
-}
+//     return (<tr>
+//         <td>{price}</td>
+//         <td>{quantity}</td>
+//         <td>{price * quantity}</td>
+//         <td>{stock.stock_symbol}</td>
+//         <td><Button>Cancel</Button></td>
+//     </tr>
+//     );
+// }
 
 class SellSide extends React.Component {
+    state = {
+        displayAll: false
+    }
+
+    constructor(props){
+        super(props);
+        this.state.displayAll = false;
+    }
+
+    static defaultProps = {
+        orders: [
+            {
+                "price": 1.00,
+                "quantity": 100,
+                "symbol": "AMZN"
+
+            },
+            {
+                "price": 0.99,
+                "quantity": 110,
+                "symbol": "AMZN"
+
+            },
+            {
+                "price": 0.98,
+                "quantity": 120,
+                "symbol": "AMZN"
+
+            }
+        ]
+    }
+
 
     static defaultProps = {
         orders: [
@@ -53,27 +87,40 @@ class SellSide extends React.Component {
         ]
     }
 
+    changeDisplay(){
+        // console.log(this.state.displayAll)
+         this.setState({displayAll:!this.state.displayAll})
+     }
+
 
     render() {
         // Here is a debug method to moniter incoming contact data
         console.log("Rending Order Book:")
         console.log(this.props.orders)
+        let orderSlice
+        if(!this.state.displayAll){
+         orderSlice = this.props.orders.slice(0,5);
+        } else{
+            orderSlice=this.props.orders;
+        }
+
 
 
         return (
             <React.Fragment>
                 <h1 className="text-center">SELL</h1>
-            <Table striped bordered hover>
+            <Table striped bordered hover size="sm">
                 <thead>
                     <SellSideHeader/>
                 </thead>
                 <tbody>
-                    {this.props.orders.map((order, i) => {
-                        return <SellSideRow order={order} key={i} />
+                {orderSlice.map((order, i) => {
+                        return <SellSideRow order={order} key={i} cancelOrder={this.props.cancelOrder} updateOrder={this.props.updateOrder} />
                     })}
 
                 </tbody>
             </Table>
+            <Button onClick={ () => this.changeDisplay()}>{this.state.displayAll? <p>Show {this.props.orders.length -5} less orders</p> : <p>Show {this.props.orders.length -5} more orders</p> }</Button>
             </React.Fragment>)
     }
 }
---
+++ Front_End/order-book/src/Components/SellSideRow.js
@@ -0,0 +1,177 @@
+import React from "react";
+import { Table, Button, OverlayTrigger, Tooltip } from "react-bootstrap";
+import OrderAdjuster from "./OrderAdjuster";
+import styled from "styled-components";
+
+const RedText = styled.p`
+  color: #ed1212;
+`;
+
+const GreenText = styled.p`
+  color: #008000;
+`;
+
+class SellSideRow extends React.Component {
+  state = {
+    editOrder: {
+      quantity: "",
+      price: "",
+    },
+  };
+
+  constructor(props) {
+    super(props);
+    this.state.editOrder.quantity =
+      this.props.order.order_size - this.props.order.number_matched;
+    this.state.editOrder.price = this.props.order.price;
+    this.state.editOrder.total =
+      this.props.order.price * this.state.editOrder.quantity;
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.order.id !== prevProps.order.id) {
+      //   let quantity = this.props.order.order_size - this.props.order.number_matched;
+      //   let price = this.props.order.price;
+      let newOrderData = {
+        quantity: this.props.order.order_size - this.props.order.number_matched,
+        price: this.props.order.price,
+      };
+
+      this.setState({ editOrder: newOrderData });
+    }
+  }
+
+  resetEdit = () => {
+    let newOrderData = {
+      quantity: this.props.order.order_size - this.props.order.number_matched,
+      price: this.props.order.price,
+    };
+
+    this.setState({ editOrder: newOrderData });
+  };
+
+  handleChangeNumber = (name, value, event) => {
+    let inputName = name;
+    let inputValue = value;
+    if (event.shiftKey) {
+      inputValue = value * 5;
+    }
+
+    let orderData = this.state.editOrder;
+
+    if (orderData.hasOwnProperty(inputName)) {
+      console.log(orderData[inputName]);
+      console.log(inputValue);
+      orderData[inputName] += inputValue;
+      if(orderData[inputName] < 0){
+        orderData[inputName] = 0;
+      }
+
+
+      this.setState({ editOrder: orderData });
+    }
+  };
+
+  render() {
+    let {
+      price,
+      order_size,
+      number_matched,
+      side,
+      stock,
+      id,
+      placed_at,
+      status,
+      user,
+    } = this.props.order;
+    let quantity = order_size - number_matched;
+    let total = price * quantity;
+    let sum = this.state.editOrder.price * this.state.editOrder.quantity;
+    console.log("value of id in buyside row is :" + id.id + " " + id.version);
+
+    let editTotal = (Math.round(sum * 100) / 100).toFixed(2);
+
+    const renderTooltip = (props) => (
+      <Tooltip id="button-tooltip" {...props}>
+        Simple tooltip
+      </Tooltip>
+    );
+
+    return (
+      <tr>
+        <td>
+          <OrderAdjuster
+            value={price}
+            editValue={this.state.editOrder.price}
+            handleChangeNumber={this.handleChangeNumber}
+            name={"price"}
+            tickSize={0.01}
+          />
+        </td>
+
+        <td>
+          <OrderAdjuster
+            value={quantity}
+            editValue={this.state.editOrder.quantity}
+            handleChangeNumber={this.handleChangeNumber}
+            name={"quantity"}
+            tickSize={1}
+          />
+        </td>
+        <td>
+          {total}
+          {editTotal > total ? <GreenText> {editTotal}</GreenText> : null}
+          {editTotal < total ? <RedText> {editTotal}</RedText> : null}
+        </td>
+        <td>{stock.stock_symbol}</td>
+        <td>{id.id}</td>
+        
+
+        <td>
+          <Button
+            size="sm"
+            data-id={id.id}
+            data-version={id.version}
+            onClick={this.props.cancelOrder}
+          >
+            Cancel
+          </Button>
+          {this.state.editOrder.quantity != quantity ||
+          this.state.editOrder.price != price ? (
+            <React.Fragment>
+              {" "}
+              <Button
+                size="sm"
+                data-id={id.id}
+                data-version={id.version}
+                data-price={this.state.editOrder.price}
+                data-order_size={this.state.editOrder.quantity +number_matched}
+                data-number_matched={number_matched}
+                data-side={side}
+                data-placed_at={placed_at}
+                data-status={status}
+                data-usr_id={user.id}
+                data-stock_id={stock.id}
+                onClick={this.props.updateOrder}
+              >
+                Update
+              </Button>
+              <Button size="sm" onClick={this.resetEdit}>
+                Reset
+              </Button>
+            </React.Fragment>
+          ) : null}
+          <OverlayTrigger
+            placement="right"
+            delay={{ show: 250, hide: 400 }}
+            overlay={renderTooltip}
+          >
+            <Button variant="success"></Button>
+          </OverlayTrigger>
+        </td>
+      </tr>
+    );
+  }
+}
+
+export default SellSideRow;
